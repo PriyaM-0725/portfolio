b0161712ab56da35395a3070e0cc882bdec10e90
PriyaM-0725 <priyamoorthy0725@gmail.com>
2025-07-12 14:43:00 +0530

    white box remove

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,9 @@ import Header from '@/components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-black text-white font-sans">
       <Header />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
